592623fbdc14e456d6cc84dd924082ea29252228
Paulo Henrique <paulo.junior@laura-br.com>
2020-04-22 20:17:32 -0300

    Updates

---
+++ precache-manifest.329c2da382879e5af6c1e39440d767d3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "711d2a99208208fc9e400bcb8bf0f733",
+    "url": "/rockstar-movies-front/index.html"
+  },
+  {
+    "revision": "ff393c0d408a43ff4cbc",
+    "url": "/rockstar-movies-front/static/css/2.59405167.chunk.css"
+  },
+  {
+    "revision": "6ab6af1c27de746c5439",
+    "url": "/rockstar-movies-front/static/css/main.45dc0d24.chunk.css"
+  },
+  {
+    "revision": "ff393c0d408a43ff4cbc",
+    "url": "/rockstar-movies-front/static/js/2.78b709dd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/rockstar-movies-front/static/js/2.78b709dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ab6af1c27de746c5439",
+    "url": "/rockstar-movies-front/static/js/main.3cfc996b.chunk.js"
+  },
+  {
+    "revision": "d3f29d093911a6bc2b74",
+    "url": "/rockstar-movies-front/static/js/runtime-main.3bace94d.js"
+  }
+]);
